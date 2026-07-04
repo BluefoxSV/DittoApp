e55124b581be26d6094bd4a94486ddebcc7da7f0
KevinDavidSilva <elmersilva462@gmail.com>
2026-07-04 15:26:44 -0600

    detele style 1126

--- ditto-app/src/pages/dashboards/lazy.jsx
+++ ditto-app/src/pages/dashboards/lazy.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from 'react';
 
-const userDashboard = lazy(() => import('./HomePage.jsx'));
-const workerDashboard = lazy(() => import('./HomePage.jsx'));
+const userDashboard = lazy(() => import('./userdash.jsx'));
+const workerDashboard = lazy(() => import('./workerdash.jsx'));
 
 const userDashboardLazy = {
   routes: [
