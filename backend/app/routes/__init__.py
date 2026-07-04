1fd51489f824186300e2171d84c1e5e1894500cc
asael <asaelgdiaz@gmail.com>
2026-07-04 15:59:35 -0600

    update routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.routes import chat, courses, service_requests, users, workers
 
-api_router = APIRouter(prefix="/api/v1")
+api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(workers.router)
 api_router.include_router(courses.router)
