9375e7a9ccb9365120d7b0aae9c7caefb8303d46
Luk <gisrael028@gmail.com>
2026-07-05 03:19:45 -0600

    se modifica ditto-app/src/store/store.js

--- ditto-app/src/store/store.js
+++ ditto-app/src/store/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import "./api/authApi";
 import "./api/usersApi";
+import "./api/chatApi";
 import "./api/elevenlabsApi";
 import authReducer from "./slices/authSlice";
 
