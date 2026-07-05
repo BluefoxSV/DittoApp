09801b68443da583f3f9c836a26f263f80e80e36
Luk <gisrael028@gmail.com>
2026-07-05 03:30:47 -0600

    se modifica ditto-app/src/store/api/elevenlabsApi.js

--- ditto-app/src/store/api/elevenlabsApi.js
+++ ditto-app/src/store/api/elevenlabsApi.js
@@ -2,10 +2,18 @@ import { apiSlice } from "./apiSlice";
 
 export const elevenlabsApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    getConversationConfig: builder.query({
+      query: () => "/elevenlabs/conversation-config",
+    }),
     getSignedUrl: builder.query({
       query: () => "/elevenlabs/signed-url",
     }),
   }),
 });
 
-export const { useGetSignedUrlQuery, useLazyGetSignedUrlQuery } = elevenlabsApi;
+export const {
+  useGetConversationConfigQuery,
+  useLazyGetConversationConfigQuery,
+  useGetSignedUrlQuery,
+  useLazyGetSignedUrlQuery,
+} = elevenlabsApi;
