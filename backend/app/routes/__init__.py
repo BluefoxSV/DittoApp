66772188404cae8bb4bd0be2147fb5b05a8ab80f
asael <asaelgdiaz@gmail.com>
2026-07-04 18:28:13 -0600

    add /api prefix

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.routes import auth, chat, courses, service_requests, users, workers
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api")
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(workers.router)
